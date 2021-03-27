64dc8a67c77592263b2dd72b05908e51df497f38
w864s382 <w864s382@ku.edu>
2021-03-26 19:13:59 -0500

    added character.js which currently includes a constructor and 4 types of attacks/healing, aswell as damage multipliers and armor protection, also inclues mana drain from attacker, everything is made to be easy to change in the future as values need tweaking

---
+++ character.js
@@ -0,0 +1,57 @@
+var mM = 1;//manaMultiplier
+var dM = 1;//damageMultiplier
+
+//character creator used "a1 = new character"
+function character(m_health, m_magic, m_armor){
+	this.health = m_health;
+	this.max_health = m_health;
+	this.magic = m_magic;
+	this.max_magic = m_magic;
+	this.armor = m_armor;
+}
+
+//function to calculate and apply damage to defender, input is base damage
+function applyDamage(defender,damage){
+	if(defender.armor>(dM*damage)){}
+	else{
+		defender.health = defender.health - (dM*damage) + defender.armor;
+	}
+}
+
+//heals defender, makes sure no overheal
+function applyHeal(defender,damage){
+	if(((damage*dM)+defender.health)>defender.max_health){
+		defender.health = defender.max_health;
+	}
+	else{
+		defender.health = defender.health + (damage*dM);
+	}
+}
+
+
+//takes magic from attacker, input base mana
+function applyMagic(attacker,mana){
+	attacker.magic = attacker.magic - (mM*mana);
+}
+
+function single(attacker, defender, damage, mana){
+	applyDamage(defender,damage);
+	applyMagic(attacker,mana);
+}
+
+function aoe(attacker, group, damage, mana){
+	group.map(x=>applyDamage(x,damage));
+	applyMagic(attacker,mana);	
+}
+
+function heal(attacker, defender, damage, mana){//I know this doesnt make sense but im keeping the variables the same for consistancy
+	applyHeal(defender,damage);
+	applyMagic(attacker,mana);	
+}
+
+function groupHeal(attacker, group, damage, mana){
+	group.map(x=>applyHeal(x,damage));
+	applyMagic(attacker,mana);
+}
+
+
