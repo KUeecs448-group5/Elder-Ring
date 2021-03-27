60b7d38431e4983c9e8f5a1629e9c67474d2f8a1
Caden Kroonenberg <cskroonenberg@gmail.com>
2021-03-27 13:57:51 -0500

    Implement enemy action function in game.js

--- game.js
+++ game.js
@@ -1,22 +1,22 @@
 //overall game function
 function newGame(){
-    const characterArray = []; //player character array
+    const playerArray = []; //player character array
     const enemyArray = []; //enemy character array
-    for(let i = 0; i < 4; i++){
-        characterArray[i] = new character(100, 100, 50);
+    for(let i = 0; i < 4; i++){ //create 3 player characters
+        playerArray[i] = new character(100, 100, 50);
     }
-    for(let i = 0; i < 4; i++){
-        enemyArray[i] = new character(80, 80, 80);
+    for(let i = 0; i < 4; i++){ //create 3 enemy characters
+        enemyArray[i] = new character(80, 100, 100);
     }
     
     do {
         for(let i = 0; i < 4; i++){
             playerAction(/*parameters*/);
-            //check win condition
+            //check win condition -> return from newGame function if true
         }
         for(let i = 0; i < 4; i++){
             enemyAction(enemyArray[i]);
-            //check win condition
+            //check win condition -> return from newGame function if true
         }
     } while(1);
 }
@@ -25,8 +25,20 @@ function playerAction(/*parameters*/){
 
 }
 
-function enemyAction(/*parameters*/){
-    
+function enemyAction(toAct){
+    action = Math.floor(Math.random() * 4);
+    target = Math.floor(Math.random() * 3);
+    pseudoMultiplier = Math.random() * 10;
+    if(select === 0){
+        toAct.single(toAct, playerArray[target], 25 + pseudoMultiplier, 10);
+    } else if(select === 1){
+        toAct.aoe(toAct, playerArray, 10 + pseudoMultiplier, 15);
+    } else if(select === 2){
+        toAct.heal(toAct, enemyArray[target], 5 + pseudoMultiplier, 15);
+    } else if(select === 3){
+    } else {
+        console.log("Random enemy action selection failure");
+    }
 }
 
 function checkWin(/*parameters*/){
