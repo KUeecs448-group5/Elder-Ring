0901b3a9c1de4a1917f9b554cff81c1bf1eaae07
Steven0710 <stevenhujuly10@gmail.com>
2021-04-02 17:52:05 -0500

    buttons work one at a time now

--- game.js
+++ game.js
@@ -2,15 +2,11 @@
 import Character from './character.js';
 //var playerAction2=setInterval(playerAction,3000);
 //var enemyAction2=setInterval(enemyAction,3000);
-function newGame(){
     const playerArray = []; //player character array
     const enemyArray = []; //enemy character array
-    playerArray[0] = new Character(100,100,0,"Archer",0);
-    playerArray[1] = new Character(100,100,0,"Barbarian",1);
-    playerArray[2] = new Character(100,100,0,"Mage",2);
-    enemyArray[0] = new Character(80,100,100,"Bad Guy",1);
-    enemyArray[1] = new Character(80,100,100,"Bad Guy 2",2);
-    enemyArray[2] = new Character(80,100,100,"Bad Guy 3",3);
+    
+function newGame(){
+    
     // for(let i = 0; i < 4; i++){ //create 3 player characters
     //     playerArray[i] = new Character(100, 100, 50);
     // }
@@ -19,41 +15,50 @@ function newGame(){
     // }
 
     //attack all players with 0 to get health bars started
-
-    let playerSelection = prompt("who would you like to attack with?");
-    let enemySelection = prompt("who would you like to attack?(For Heal,type 3)");
-    let playerSelection2 = parseInt(playerSelection);
-    let enemySelection2 = parseInt(enemySelection);
-    if(enemySelection2 == 3){
-        enemySelection = prompt("Who in your party would you like to heal?");
-        enemySelection2 = parseInt(enemySelection);
-    }
-    //playerAction(playerArray,playerArray,playerSelection2,enemySelection2);
-    do {
-        /*
-        for(let i = 0; i < playerArray.length; i++){
-            playerAction(playerArray,playerArray,playerSelection2,enemySelection2);
-            updateArray(enemyArray); //if an enemy character dies, they will be removed from the enemy array
-            if(checkWin(enemyArray) || checkWin(playerAction)){ //check win condition -> return from newGame function if true
-                return;
-            }
-        }
-        */
-        for(let i = 0; i < enemyArray.length; i++){
-            enemyAction(enemyArray, playerArray, enemyArray[i]);
-            updateArray(playerArray); //if a player charater dies, they will be removed from the player array
-            /*if(checkWin(enemyArray) || checkWin(playerAction)){ //check win condition -> return from newGame function if true
-                return;
-            }*/
-        }
-    } while(1);
+    playerArray[0] = new Character(80,100,0,"Archer",3);
+    playerArray[1] = new Character(100,100,0,"Barbarian",4);
+    playerArray[2] = new Character(100,100,0,"Mage",5);
+    enemyArray[0] = new Character(100,100,100,"Bad Guy 1",0);
+    enemyArray[1] = new Character(100,100,100,"Bad Guy 2",1);
+    enemyArray[2] = new Character(100,100,100,"Bad Guy 3",2);
+    
+    document.getElementById("Attack").addEventListener("click", attackAction);
+    document.getElementById("AOE").addEventListener("click", aoeAction);
+    document.getElementById("Heal").addEventListener("click", healAction);
 }
 
+function attackAction(){
+    let playerSelection = parseInt(prompt("who would you like to attack with?"));
+    let enemySelection = parseInt(prompt("who would you like to attack?(For Heal,type 3)"));
+   
+    if(enemySelection == 3){
+        enemySelection = parseInt(prompt("Who in your party would you like to heal?"));
+    }
+    let randomEnemyIndex = Math.floor(Math.random() * playerArray.length);
+    playerArray[playerSelection].singlePlayer(playerArray[playerSelection],enemyArray[enemySelection])
+    enemyAction(enemyArray, playerArray, enemyArray[randomEnemyIndex]);
+    updateArray(playerArray);
+    updateArray(enemyArray);
+
+}
+
+function aoeAction(){
+    let playerSelection = parseInt(prompt("who would you like to attack with?"));
+    let randomEnemyIndex = Math.floor(Math.random() * playerArray.length);
+    playerArray[playerSelection].aoePlayer(playerArray[playerSelection],enemyArray)
+    //enemyAction(enemyArray, playerArray, enemyArray[randomEnemyIndex]);
+    updateArray(playerArray);
+    updateArray(enemyArray);
+
+}
+
+function healAction(){
+    let playerSelection = parseInt(prompt("who would you like to use heal?"));
+    let allySelection = parseInt(prompt("who would you like heal to be used on?"));
+    playerArray[playerSelection].healPlayer(playerArray[playerSelection],playerArray[allySelection]);
+}
 //figure out why this repeats
 function playerAction(playerArray,enemyArray,player,enemy){
-    document.getElementById("Attack").addEventListener
-    ("click", () => 
-    playerArray[player].singlePlayer(playerArray[player],enemyArray[enemy]));
     
     document.getElementById("AOE").addEventListener
     ("click", () => 
@@ -70,13 +75,12 @@ function enemyAction(enemyArray, playerArray, toAct){
     //need to validate target - cannot attack dead player or heal an ally at full health
     var pseudoMultiplier = Math.random() * 10;
     console.log("Enemy health: " + toAct.health);
-    console.log("Enemy action: " + action);
+    
     console.log("playerArray.length: " + playerArray.length);
     if(action === 0){
         //toAct.single(toAct, playerArray[target], 25 + pseudoMultiplier, 10);
         toAct.singleEnemy(toAct, playerArray[target]);
-        console.log("Enemy is attacking player " + target + ".");
-        console.log("Player " + target + " health: " + playerArray[target].heath);
+        console.log("Player " + target + " health: " + playerArray[target].health);
     } else if(action === 1){
         //toAct.aoe(toAct, playerArray, 10 + pseudoMultiplier, 15);
         toAct.aoeEnemy(toAct, playerArray);
