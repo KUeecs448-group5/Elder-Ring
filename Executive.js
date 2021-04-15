519b3aacb12ea1c39ae62eebf92e76c545fba6f6
w864s382 <w864s382@ku.edu>
2021-04-15 17:51:26 -0500

    added protection for the page not being loaded

--- Executive.js
+++ Executive.js
@@ -4,9 +4,11 @@ import * as gameRules from './game.js';
 
 var menu = document.getElementById("start");
 var debug = false;
-menu.onclick = function() {
-    console.log("Clicked")
-    menu.style.visibility  = "hidden";
-    gameRules.newGame(1);//start world 0/1
-}
+document.addEventListener("DOMContentLoaded", ()=>{
+    menu.onclick = function() {
+        console.log("Clicked")
+        menu.style.visibility  = "hidden";
+        gameRules.newGame(1);//start world 0/1
+    }
+})    
 export default debug;
