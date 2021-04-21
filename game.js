111386cad32cc07916540ea8be70231f5842dc51
w864s382 <w864s382@ku.edu>
2021-04-21 02:23:27 -0500

    found some darksouls assets

--- game.js
+++ game.js
@@ -127,8 +127,6 @@ function playerAction(playerArray,enemyArray,player){
                     aoe.onclick = function(){};
                     heal.onclick = function(){};
                     item.onclick = function(){};                    
-           // aoe.onclick = async function(){
-                    console.clear();
                     playerArray[player].damage_single(enemyArray[i],values[0]);
                     charId[player+3].src = bANattack[gameMode][player];
                     await sleep(4000);
