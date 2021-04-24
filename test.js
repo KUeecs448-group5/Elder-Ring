b14ba52e19a4c1d9b8159a58f7e1a218147aa951
Steven0710 <stevenhujuly10@gmail.com>
2021-04-23 21:28:43 -0500

    reorganized test functions

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ export {testAll}
 
 const testplayerArray = []
 const testenemyArray = []
+let status = "Failed"
 
 function testAll(){
     testplayerArray[0] = new Character(100,100,names[0][0],3);
@@ -14,15 +15,14 @@ function testAll(){
     testenemyArray[1] = new Character(200,100,enames[0][1],1);
     testenemyArray[2] = new Character(100,100,enames[0][2],2);
     testSingleAttack()
-    testAoE()
     testHeal()
     testItem()
+    testAoE()
 
 }
 function testSingleAttack(){
    
     testplayerArray[0].damage_single(testenemyArray[0],values[0])
-    let status = "Failed"
     if(testenemyArray[0].health != 100 && testplayerArray[0].magic < 100){
         status = "Passed"
     }
@@ -30,29 +30,33 @@ function testSingleAttack(){
 }
 
 
-function testAoE(){
-    testplayerArray[0].damage(testenemyArray,values[1]);
-    status = "Failed"
-    if(testenemyArray[0].health < 100 && testenemyArray[1].health < 200 && testenemyArray[2].health < 100 && testplayerArray[0].magic < 100){
-        status = "Passed"
-    }
-    console.log("AoE Attack Test(damaged enemy and player's mana reduced): " + status)
-}
-
 function testHeal(){
-    testplayerArray[0].heal_single(testplayerArray[0],values[2]);
+    testplayerArray[1].health = 90
+    testplayerArray[1].heal_single(testplayerArray[1],values[2]);
     status = "Failed"
-    if(testplayerArray[0].health >90 && testplayerArray[0].magic <100){
+    if(testplayerArray[1].health >90 && testplayerArray[1].magic <100){
         status = "Passed"
     }
     console.log("Heal Test(party member healed and player's mana reduced): " + status)
 }
 
 function testItem(){
-    testplayerArray[0].useItem(testenemyArray[0],values[3]);
+    testplayerArray[0].useItem(testenemyArray[1],values[3]);
     status = "Failed"
-    if(testenemyArray[0].health !=100 && testplayerArray[0].item == 2){
+    if(testenemyArray[0].health <=100 && testplayerArray[0].item == 2){
         status = "Passed"
     }
     console.log("Item Test(enemy damaged and player inventory reduced by 1): " + status)
+}
+
+function testAoE(){
+    testenemyArray[0].health = 100
+    testenemyArray[1].health = 200
+    testenemyArray[2].health = 100
+    testplayerArray[1].damage(testenemyArray,values[1]);
+    status = "Failed"
+    if(testenemyArray[0].health < 100 && testenemyArray[1].health < 200 && testenemyArray[2].health < 100 && testplayerArray[0].magic < 100){
+        status = "Passed"
+    }
+    console.log("AoE Attack Test(damaged enemy and player's mana reduced): " + status)
 }
